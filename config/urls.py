289836adf6746bffee112e84dd3ce51a8743d3be
jyp <bjy6256@naver.com>
2023-06-20 14:47:01 +0900

    study : 3-10 views.py 파일 분리

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pybo import views
+from pybo.views import base_views
 
 # from pybo import views
 
@@ -25,5 +25,5 @@ urlpatterns = [
     # path('pybo/', views.index),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
-    path('', views.index, name='index'),  # '/' 에 해당되는 path
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]
